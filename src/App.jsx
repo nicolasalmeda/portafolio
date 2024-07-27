7c71d1505dfa3edf9a28e627b06ca5602bac75cd
Nicolas Ala <nicoalmeda96@gmail.com>
2024-07-26 23:06:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './App.css'
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import CircleLoader from "react-spinners/CircleLoader";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import MoveToTop from "./components/MoveToTop";
+import Nav from "./components/Nav";
+import Projects from "./components/Projects";
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1900);
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div className="loader">
+          <CircleLoader
+            color={"#011c38"}
+            loading={true}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <>
+          <Nav />
+          <MoveToTop />
+
+          <TransitionGroup className="">
+            <CSSTransition key={location.key} classNames="fade" timeout={500}>
+              <Routes location={location}>
+                <Route path="/" element={<Home />} />
+                <Route path="/Projects" element={<Projects />} />
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
+
+          <Footer />
+        </>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import {AiOutlineHome} from 'react-icons/ai'
+import {BsPerson, BsCodeSlash} from 'react-icons/bs'
+import logo from '../assets/img/logo.png'
+import '../css/nav.css'
+
+const Nav = () => {
+
+  const [navBarBlur, setNavBarBlur] = useState(false)
+
+  const handleScroll = () => {
+    if (window.scrollY >= 20) {
+      setNavBarBlur(true)
+    } else {
+      setNavBarBlur(false)
+    }
+  }
+
+  let showMenu = () => {
+    let bar = document.getElementsByClassName('bar');
+    let ham = document.getElementsByClassName('NavbarLinks');
+    bar[0].classList.toggle('barOne');
+    bar[1].classList.toggle('barTwo');
+    bar[2].classList.toggle('barThree');
+
+    ham[0].classList.toggle('showNavbar');    
+  }
+
+  let hideMenu = () => {
+    let bar = document.getElementsByClassName('bar');
+    let ham = document.getElementsByClassName('NavbarLinks');
+    bar[0].classList.remove('barOne');
+    bar[1].classList.remove('barTwo');
+    bar[2].classList.remove('barThree');
+
+    ham[0].classList.remove('showNavbar');
+  }
+
+  window.addEventListener('scroll', handleScroll)
+  return (
+    <nav className={navBarBlur ? "Navbar blur" : "Navbar"}>
+    <h1
+      title="Reload"
+      onClick={() => window.location.reload(true)}
+      className="Logo"
+    >
+      <img className='logoImg' src={logo}/>
+    </h1>
+
+    <div className="Hamburger" onClick={showMenu}>
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
+    </div>
+
+    <ul className="NavbarLinks">
+      <li onClick={hideMenu}>
+        <Link to="/">
+          <AiOutlineHome /> Home
+        </Link>
+      </li>
+      <li onClick={hideMenu}>
+        <Link to="/About">
+          <BsPerson /> About
+        </Link>
+      </li>
+      <li onClick={hideMenu}>
+        <Link to="/Project">
+          <BsCodeSlash /> Projects
+        </Link>
+      </li>
+    </ul>
+  </nav>
+  )
+}
+
+export default Nav
